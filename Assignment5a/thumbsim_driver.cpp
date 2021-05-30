5656ab71ad5607a468cfdb76c7f18171c436dd52
Diego G <dcgaray01@gmail.com>
2021-05-29 18:51:36 -0700

    cache stats work now
    no idea what did it, but thank fucking god this is over

--- Assignment5a/thumbsim_driver.cpp
+++ Assignment5a/thumbsim_driver.cpp
@@ -109,7 +109,20 @@ void Memory<Data32, Data32>::dump(DataType dt) const {
 // cache size in blocks). You should also update the "hits" and
 // "misses" counters.
 bool Cache::access(unsigned int address) {
-  return false;
+  unsigned int offset = log2(blocksize);
+  unsigned int index = log2(entries.size());
+  address = address >> offset;
+  unsigned int current = address %  entries.size();
+  unsigned int tag = address >> index;
+
+  if (entries[current] == tag) {
+        hits++;
+        return true;
+    } else { 
+        misses++;
+        entries[current] = tag;
+        return false;
+    }
 }
 
 void Stats::print() {
